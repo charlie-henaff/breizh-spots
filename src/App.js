38c7cacc950defba9c47bf826ce973ffe47af903
Charlie Henaff <charlie.henaff@gmail.com>
2022-01-21 13:29:13 +0100

    Reset from gitlab src

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import './App.css';
 import { Component } from 'react';
-import { Box, Grid, Paper } from '@material-ui/core';
-import ReactHlsPlayer from 'react-hls-player'
-import ShakaPlayer from 'shaka-player-react';
-import 'shaka-player/dist/controls.css';
-
+import { Box, Grid, IconButton, Paper } from '@material-ui/core';
+import ReactPlayer from 'react-player'
+import PlayIcon from '@material-ui/icons/PlayArrowRounded';
+import PauseIcon from '@material-ui/icons/PauseRounded';
 
 class App extends Component {
 
-  addYoutubeGridItem(name, youtubeId) {
-    return (
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <Paper elevation={3}>
-          <Box p={1}>
-            <div
-              style={{
-                position: "relative",
-                paddingBottom: "56.25%" /* 16:9 */,
-                paddingTop: 25,
-                height: 0
-              }}>
-              <iframe
-                className="youtubeVideo"
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  width: "100%",
-                  height: "100%"
-                }}
-                title={name} frameborder="0"
-                src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1&mute=1&enablejsapi=1`}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen="0" />
-            </div>
-            <Box m={1}><a target="_blank" rel="noopener noreferrer" href={`https://www.google.fr/maps/search/${name}`}><b>{name}</b></a></Box>
-          </Box>
-        </Paper>
-      </Grid>
-    );
+  constructor (props) {
+  	super(props)
+    this.state = {
+    	playing: false
+    }
   }
 
-  addHlsPlayerGridItem(name, src) {
+  addPlayerGridItem(name, url) {
     return (
       <Grid item xs={12} sm={6} md={4} lg={3}>
         <Paper elevation={3}>
@@ -53,22 +26,19 @@ class App extends Component {
                 paddingTop: 25,
                 height: 0
               }}>
-              <ReactHlsPlayer
+              <ReactPlayer
+                width="100%"
+                height="100%"
                 style={{
                   position: "absolute",
                   top: 0,
                   left: 0,
-                  width: "100%",
-                  height: "100%"
                 }}
-                hlsConfig={{
-                  maxLoadingDelay: 4,
-                  minAutoBitrate: 0,
-                  lowLatencyMode: true,
-                }}
-                autoPlay={true}
+                url={url}
+                playing={this.state.playing}
+                muted={true}
                 controls={true}
-                src={src} />
+                onError={(e)=> console.log(e)} />
             </div>
             <Box m={1}><a target="_blank" rel="noopener noreferrer" href={`https://www.google.fr/maps/search/${name}`}><b>{name}</b></a></Box>
           </Box>
@@ -76,64 +46,46 @@ class App extends Component {
       </Grid>
     );
   }
-
-  addMdpPlayerGridItem(name, src) {
-    return (
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <Paper elevation={3}>
-          <Box p={1}>
-            <div
-              style={{
-                position: "relative",
-                paddingBottom: "56.25%" /* 16:9 */,
-                paddingTop: 25,
-                height: 0
-              }}>
-              <ShakaPlayer
-                autoPlay
-                src={src} />
-            </div>
-            <Box m={1}><a target="_blank" rel="noopener noreferrer" href={`https://www.google.fr/maps/search/${name}`}><b>{name}</b></a></Box>
-          </Box>
-        </Paper>
-      </Grid>
-    );
-  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>North breizh spots live</h2>
+          <h2>Breizh spots live</h2>
+          <div className="App-header-actions">
+            <IconButton color="secondary" onClick={() => this.setState({playing: !this.state.playing})}>
+              {this.state.playing ? <PauseIcon/> : <PlayIcon/>}
+            </IconButton>
+          </div>
         </header>
         <div className="App-content">
           <Grid container spacing={3}>
             {/* Perros-Guirec */}
-            {this.addYoutubeGridItem("Perros-Guirec", "ui_9qAoosls")}
+            {this.addPlayerGridItem("Perros-Guirec", "https://www.youtube.com/watch?v=ui_9qAoosls")}
 
             {/* Trestraou */}
-            {this.addYoutubeGridItem("Trestraou", "rmVJ1Uls9AM")}
+            {this.addPlayerGridItem("Trestraou", "https://youtu.be/_5kHwxpR3tA")}
 
             {/* Saint-Quay-Portieux */}
-            {this.addYoutubeGridItem("Saint-Quay-Portieux", "lWPgeICToUk")}
+            {this.addPlayerGridItem("Saint-Quay-Portieux", "https://www.youtube.com/watch?v=lWPgeICToUk")}
 
             {/* Binic */}
-            {this.addYoutubeGridItem("Binic", "8DThsr2fxwk")}
+            {this.addPlayerGridItem("Binic", "https://www.youtube.com/watch?v=8DThsr2fxwk")}
 
-            {/* Pléneuf */}
-            {this.addHlsPlayerGridItem("Pléneuf", 'https://hls.iwilive.net/live/pleneuf0.stream/chunklist_w1311621573.m3u8')}
+            {/* Pléneuf Val André */}
+            {this.addPlayerGridItem("Pléneuf-Val-André", 'https://hls.iwilive.net/live/pleneuf0.stream/chunklist_w1311621573.m3u8')}
 
             {/* SAINT-CAST-LE GUILDO */}
-            {this.addHlsPlayerGridItem("SAINT-CAST-LE GUILDO", "https://hls.iwilive.net/live/stcast0.stream/chunklist_w382338439.m3u8")}
+            {/* {this.addPlayerGridItem("SAINT-CAST-LE GUILDO", "https://hls.iwilive.net/live/stcast0.stream/chunklist_w382338439.m3u8")} */}
 
             {/* Dinard */}
-            {this.addMdpPlayerGridItem("Dinard", "https://deliverys4.joada.net/contents/encodings/vod/b751a540-393a-485c-3539-3430-6d61-63-af54-f2141dcc23ead/mpd.mpd")}
+            {/* {this.addPlayerGridItem("Dinard", "https://deliverys4.joada.net/contents/encodings/vod/d0ea84ed-5a5e-4c06-3539-3430-6d61-63-9bf8-d855548eb34ed/mpd.mpd")} */}
 
             {/* Saint-Malo */}
-            {this.addYoutubeGridItem("Saint-Malo", "OetL01QjfBs")}
+            {this.addPlayerGridItem("Saint-Malo", "https://youtu.be/2uznaTjvb7I")}
 
             {/* Cancale */}
-            {this.addYoutubeGridItem("Cancale", "o06T213KolE")}
+            {this.addPlayerGridItem("Cancale", "https://www.youtube.com/watch?v=o06T213KolE")}
 
           </Grid>
         </div>
